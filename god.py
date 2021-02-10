7e1d0593334765cfc8a44c8730f2cf3612b8e59c
Солнышко <nemina.pe@yandex.ru>
2021-02-10 23:07:36 +0500

    обработка клавиш вверх/вниз/вправо/влево

--- god.py
+++ god.py
@@ -1,60 +1,71 @@
-import os
+import math
 import sys
 
 import pygame
 import requests
 
+ZOOM = 17
+x, y = 59.9386, 30.3141
 
-class Maps:
-    def __init__(self, x, y, masshtab):
-        self.x = x
-        self.y = y
-        self.masshtab = masshtab
 
-    def update(self):
-        self.map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.y},{self.x}&z={self.masshtab}&l=map&size=650,450"
-        self.response = requests.get(self.map_request)
-        self.wrong(self.response)
+def map_write():
+    map_file = "map.png"
+    map_request = f"https://static-maps.yandex.ru/1.x/?ll={y},{x}&z={ZOOM}&size=450,450&l=map"
+    response = requests.get(map_request)
 
-    def wrong(self, response):
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print(self.map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
 
-    def write_picture(self):
     # Запишем полученное изображение в файл.
-        map_file = "map.png"
-        with open(map_file, "wb") as file:
-            file.write(response.content)
+
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
 
 # Инициализируем pygame
 pygame.init()
-screen = pygame.display.set_mode((650, 450))
+screen = pygame.display.set_mode((450, 450))
 # Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
+map_write()
+screen.blit(pygame.image.load("map.png"), (0, 0))
 # Переключаем экран и ждем закрытия окна.
-x, y, masshtab = 60.01, 29.71, 8
-map = Maps(x, y, masshtab)
 running = True
+pygame.display.flip()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        move = ''
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEUP:
+                if ZOOM + 1 <= 17:
+                    ZOOM += 1
+                    map_write()
+            if event.key == pygame.K_PAGEDOWN:
+                if ZOOM - 1 >= 0:
+                    ZOOM -= 1
+                    map_write()
+            if event.key == pygame.K_UP:
+                x_1 = 256 / 2 * math.pi * 2 ** ZOOM * (math.pi + math.radians(y))
+                x += (450 * x_1 / 10 ** 8) / 10 ** 5.55
+                map_write()
+            if event.key == pygame.K_DOWN:
+                x_1 = 256 / 2 * math.pi * 2 ** ZOOM * (math.pi + math.radians(y))
+                x -= (450 * x_1 / 10 ** 8) / 10 ** 5.55
+                map_write()
+            if event.key == pygame.K_LEFT:
+                y_1 = 360 / (math.pow(2, ZOOM + y))
+                print(y)
+                y -= 450 * y_1 * 10 ** 6.72
+                print(y)
+                map_write()
             if event.key == pygame.K_RIGHT:
-                 move = 'right'
-            elif event.key == pygame.K_LEFT:
-                move = 'left'
-            elif event.key == pygame.K_UP:
-                move = 'up'
-            elif event.key == pygame.K_DOWN:
-                move = 'down'
-
+                y_1 = 360 / (math.pow(2, ZOOM + y))
+                y += 450 * y_1 * 10 ** 6.72
+                map_write()
+    screen.blit(pygame.image.load("map.png"), (0, 0))
     pygame.display.flip()
 
 pygame.quit()
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
